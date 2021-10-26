c942c5a2669bbb05bedb95a94b97421ea55ab05a
BalamuruganThirumalaikumar <79850259+BalamuruganThirumalaikumar@users.noreply.github.com>
2021-10-26 18:34:10 +0530

    4637-how-to-set-supertooltip-for-cells

---
+++ Control ToolTip/CS/Backup/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GC_SuperTooltip")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SYNC FUSION SOFTWARE (P) LTD.")]
+[assembly: AssemblyProduct("GC_SuperTooltip")]
+[assembly: AssemblyCopyright("Copyright © SYNC FUSION SOFTWARE (P) LTD. 2007")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ba86c5c4-6c29-45b7-a8c4-7d01cfdbc9bb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
